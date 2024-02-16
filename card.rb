9f688b839198a65031dcd7aa9976a00010fb336c
jjj0331 <19980331jj@gmail.com>
2024-02-16 22:51:38 +0900

    card2

--- card.rb
+++ card.rb
@@ -2,12 +2,13 @@
 
 #=====================================================================
 class Card
-  attr_reader :cards
+  attr_reader :cards,:playercards
 
   #===================================================================== 
   def initialize 
     #トランプの各カードを配列
     @cards=[]
+    
     #カードの種類
     marks=["♦","♠","♥","🍀"]
     numbers=["1","2","3","4","5","6","7","8","9","10","11","12","13"]
@@ -15,22 +16,32 @@ class Card
     #ループでカードを作成する
     marks.each do |mark|
       numbers.each do |number|
-        @cards << mark+number
+        @cards.push(mark+number)
       end
     end
+    
+    #プレイヤーが持っているカードも初期化
+    @playercards=[]
   end
   #=====================================================================
 
     #===================================================================== 
     def Draw(numbers)
-      
-
+      for num in 1..numbers do
+        #乱数を発生させる
+        random=rand(Range.new(0, @cards.length))
+        #playercardsにカードを格納
+        @playercards.push(@cards[random])  
+        #@cardsからプレイヤーに渡されたカードを引く
+        @cards.delete_at(random)     
+      end  
     end
     #=====================================================================
 end
 
 
-cards=Card.new
-cards.cards.each do |card|
-  puts card
-end  
+trump=Card.new
+
+trump.Draw(2)
+p trump.playercards
+p trump.cards.length
